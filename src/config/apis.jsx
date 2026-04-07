9ddd0b0615d7e770a2984e110fb04f7c427d5d92
Hafiz Mahtab Afzal <hafizmahtabafzal819@gmail.com>
2026-04-07 14:36:15 +0500

    add package of vercel

--- src/config/apis.jsx
+++ src/config/apis.jsx
@@ -1,8 +1,8 @@
   
 const API = import.meta.env.VITE_API_URL;
-
+console.log("API URL:", import.meta.env.VITE_API_URL);
  const apis = {
-
+          
 
           "prod" : `${API}/api/v1/products`,
           "auth" : `${API}/api/v1/users`
